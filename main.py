5ab65ee79267e5943b67aef9bdc1b9fd5afe1f14
arbesajashanica <arbesa.jashanica@student.uantwerpen.be>
2023-04-09 23:02:03 +0200

    add classifier NB, KNN, decision tree

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder
 from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
+from xgboost import XGBClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn import tree
+import graphviz
 
 
 if __name__ == '__main__':
     # read input data and check which columns has missing values
-    existing_customers = pd.read_csv("data/existing-customers.csv", sep=';')
-    potential_customers = pd.read_csv("data/potential-customers.csv", sep=';')
+    existing_customers = pd.read_csv("data/existing-customers.csv", sep=';', index_col=0)
+    potential_customers = pd.read_csv("data/potential-customers.csv", sep=';', index_col=0)
 
     original_data = existing_customers.replace('', pd.NaT)
     original_data2 = potential_customers.replace('', pd.NaT)
@@ -24,7 +33,7 @@ if __name__ == '__main__':
 
     # convert categorical attributes to numerical attributes using the LabelEncoder
     le = LabelEncoder()
-    categoricalColumns = ['workclass', 'education', 'marital-status', 'relationship','occupation', 'race', 'sex', 'native-country']
+    categoricalColumns = ['class','workclass', 'education', 'marital-status', 'relationship','occupation', 'race', 'sex', 'native-country']
     for col in categoricalColumns:
         existing_customers[col] = le.fit_transform(existing_customers[col])
 
@@ -38,17 +47,112 @@ if __name__ == '__main__':
     scaler.fit(dataToBeNormalized)
     normalizedData = scaler.transform(dataToBeNormalized)
     existing_customers[columnsToBeNormalized] = normalizedData
-    print(existing_customers.iloc[19])
+    print(existing_customers.iloc[21389])
 
     #splitting the data in a train and test set using the train_test_split function
     features = existing_customers.iloc[:, :-1]
     label = existing_customers.iloc[:, -1]
 
-    features_train, features_test, label_train, label_test = train_test_split(features, label, test_size=0.3,random_state=42)
-    print(features_train)
-    print(features_test)
-    print(label_train)
-    print(label_test)
+    X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.3,random_state=42)
+    print(X_train)
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print(X_test)
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print(y_train)
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print(y_test)
+
+    #apply classifier1: GaussianNB()
+    gnbModel = GaussianNB()
+    gnbModel.fit(X_train, y_train)
+    y_pred = gnbModel.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("GaussianNB")
+    print(f"Accuracy: {accuracy}")
+    print("Precision:", precision_score(y_test, y_pred))
+    print("Recall:", recall_score(y_test, y_pred))
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+
+    # apply classifier2: KNeighborsClassifier(n_neighbors=5)
+    X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.3,random_state=42)
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("KNeighborsClassifier")
+    print("Accuracy:", accuracy)
+    print("Precision:", precision_score(y_test, y_pred))
+    print("Recall:", recall_score(y_test, y_pred))
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+
+    # try which k is the best
+    kValues = range(1, 20)
+    crossValisationResults = []
+    for k in kValues:
+        # Train a KNN classifier with k neighbors
+        knn = KNeighborsClassifier(n_neighbors=k)
+        # Use cross-validation to evaluate the performance of the classifier
+        X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.3, random_state=42)
+        scores = cross_val_score(knn, X_train, y_train, cv=5)
+        crossValisationResults.append(np.mean(scores))
+    plt.plot(kValues, crossValisationResults)
+    plt.xlabel('Number of neighbors')
+    plt.ylabel('Cross-validation score')
+    plt.show()
+
+    best_k = kValues[np.argmax(crossValisationResults)]
+    print('Best k:', best_k)
+
+    X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.3,random_state=42)
+    knn = KNeighborsClassifier(n_neighbors=17)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("KNeighborsClassifier 17")
+    print("Accuracy:", accuracy)
+    print("Precision:", precision_score(y_test, y_pred))
+    print("Recall:", recall_score(y_test, y_pred))
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+
+    # XGBClassifier
+    X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.3,random_state=42)
+    xgb = XGBClassifier()
+    xgb.fit(X_train, y_train)
+    y_pred = xgb.predict(X_test)
+    print("XGBClassifier")
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("Precision:", precision_score(y_test, y_pred))
+    print("Recall:", recall_score(y_test, y_pred))
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+
+    #Decision tree
+    clf = DecisionTreeClassifier(random_state=42)
+    param_grid = {'max_depth': [2, 3, 4, 5, 6, 7, 8, 9, 10]}
+    grid_search = GridSearchCV(clf, param_grid=param_grid, cv=5)
+    grid_search.fit(X_train, y_train)
+    print("Best max depth:", grid_search.best_params_['max_depth'])
+    print("Best score:", grid_search.best_score_)
+
+    clf = DecisionTreeClassifier(max_depth=3)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    accuracy = clf.score(X_test, y_test)
+    print("Accuracy:", accuracy)
+    featureNamse =  ['age', 'workclass', 'education', 'education-num', 'marital-status','occupation', 'relationship','race', 'sex','capital-gain','capital-loss', 'hours-per-week', 'native-country']
+    dot_data = export_graphviz(clf, out_file=None,
+                               feature_names=featureNamse,
+                               class_names="class",
+                               filled=True, rounded=True,
+                               special_characters=True)
+
+    # Use the GraphViz library to display the decision tree
+    graph = graphviz.Source(dot_data)
+    graph.render("decisionTree.dot")  # save as pdf
+    graph.view()
 
 
 
